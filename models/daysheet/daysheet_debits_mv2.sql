951eac21b0fb0b61929d78d07d1fc2fb78cb23a4
Alex Wickstrom <awickstrom@ucla.edu>
2024-01-19 10:03:34 -0800

    Update daysheet_debits_mv2.sql
    update schema name

--- models/daysheet/daysheet_debits_mv2.sql
+++ models/daysheet/daysheet_debits_mv2.sql
@@ -3,6 +3,8 @@
     sort = ['created_at', 'scheduled_time', 'practice_group_id', 'doctor_id'],
     auto_refresh = 'true'
 ) }}
+{% set chronometer_schema = 'chronometer_production_chronometer_production' %}
+
 SELECT
     bli.id ,
     bli.created_at ,
@@ -35,15 +37,15 @@ SELECT
     ca.institutional_claim_flag ,
     ca.appointment_status ,
     ca.reason
-FROM  {{ source('chronometer_production', 'billing_billinglineitem') }} bli
-JOIN {{ source('chronometer_production', 'chronometer_appointment') }} ca
-    ON ( bli.appointment_id = ca.id )
-JOIN {{ source('chronometer_production', 'chronometer_doctor') }} cd
-    ON ( ca.doctor_id = cd.id )
-JOIN {{ source('chronometer_production', 'chronometer_patient') }} cp
-    ON ( ca.patient_id = cp.id )
-JOIN {{ source('chronometer_production', 'chronometer_office') }} co
-    ON ( ca.office_id = co.id )
+FROM  {{ source(chronometer_schema, 'billing_billinglineitem') }} bli
+JOIN {{ source(chronometer_schema, 'chronometer_appointment') }} ca
+    ON (bli.appointment_id = ca.id)
+JOIN {{ source(chronometer_schema, 'chronometer_doctor') }} cd
+    ON (ca.doctor_id = cd.id)
+JOIN {{ source(chronometer_schema, 'chronometer_patient') }} cp
+    ON (ca.patient_id = cp.id)
+JOIN {{ source(chronometer_schema, 'chronometer_office') }} co
+    ON (ca.office_id = co.id)
 WHERE ( ca.deleted_flag IS FALSE
         AND ca.appt_is_break IS FALSE
         AND ca.is_demo_data_appointment IS FALSE
