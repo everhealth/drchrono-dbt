3223fff012753df9e721e9c897397e36dcd71ba1
Alex Wickstrom <awickstrom@ucla.edu>
2024-01-25 18:49:04 -0800

    cleanup

--- models/daysheet/credits/daysheet_credits_matched_mv.sql
+++ models/daysheet/credits/daysheet_credits_matched_mv.sql
@@ -71,5 +71,5 @@ AND NOT (
     COALESCE(lit.adjustment_group_code, '') = 'CO'
     AND lit.ins_idx = 2
 )
-AND lit.created_at >= CURRENT_DATE - INTERVAL '1 year';
+AND lit.created_at >= CURRENT_DATE - INTERVAL '1 year'
 )
