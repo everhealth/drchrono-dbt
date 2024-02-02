d85393293dedef521f82b0d94f562b66c23e1a6c
Alex Wickstrom <awickstrom@ucla.edu>
2024-02-02 12:23:28 -0800

    add space for checking model diff

--- models/intermediate/int_cashpayments.sql
+++ models/intermediate/int_cashpayments.sql
@@ -5,12 +5,11 @@ SELECT
     bcp.posted_date,
     bcp.received_date,
     bcp.created_by_id,
-    bcp.amount,
+    bcp.amount, 
     bcp.trace_number,
     bcp.payment_method,
     bcp.doctor_id,
     bcp.patient_id,
-    
     bcp.parent_id
 
 FROM {{source( 'chronometer_scrubbed', 'billing_cashpayment' ) }} bcp
