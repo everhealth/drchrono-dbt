f214c63f2cbb3bc072a312292129ef171201ba55
Alex Wickstrom <awickstrom@ucla.edu>
2024-02-02 14:41:09 -0800

    update run script
    update run command
    
    Update check_downstream_model_diff.py
    
    Update check_downstream_model_diff.py
    
    Update check_downstream_model_diff.py
    
    update run
    
    update model file & run depenedencies
    
    Update check_downstream_model_diff.py

--- .github/scripts/check_downstream_model_diff.py
+++ .github/scripts/check_downstream_model_diff.py
@@ -42,7 +42,7 @@ unique_downstream_models = set([x.get("original_file_path") for x in downstream_
 # Print models for CI/CD usage
 print("DOWNSTREAM MODELS:")
 print(' '.join(unique_downstream_models))
-
+print("Running models...\n")
 for model in unique_downstream_models:
-    subprocess.check_output(['dbt', 'run', '--select', model, '--vars', '{"schema_override": "dbt_test"}'])
+    subprocess.check_output(['dbt', 'run', '--select', str('+' + model), '--log-level', 'debug', '--vars', '{"schema_override": "dbt_test"}'])
     print(f"Finished running {model}")
--- models/intermediate/int_cashpayments.sql
+++ models/intermediate/int_cashpayments.sql
@@ -7,7 +7,6 @@ SELECT
     bcp.created_by_id,
     bcp.amount, 
     bcp.trace_number,
-    bcp.code,
     bcp.payment_method,
     bcp.doctor_id,
     bcp.patient_id,
