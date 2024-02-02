bbddeb5ce7c274b639b3fbdcfe2c3454a390cf71
Alex Wickstrom <awickstrom@ucla.edu>
2024-02-02 11:36:46 -0800

    start ci dbt run

---
+++ .github/scripts/check_downstream_model_diff.py
@@ -0,0 +1,46 @@
+import subprocess
+import json
+import os
+
+# Step 1: Identify changed files using git diff
+changed_files_output = subprocess.check_output(['git', 'diff', '--name-only', 'main...HEAD'])
+changed_files = changed_files_output.decode('utf-8').split('\n')
+
+# Filter for model SQL files
+changed_model_files = [file for file in changed_files if file.startswith('models/') and file.endswith('.sql')]
+
+# Extract model names from paths
+changed_model_names = [file.split('/')[-1].replace('.sql', '') for file in changed_model_files]
+
+# Step 2 & 3: Find downstream dependencies for each changed model
+# Assuming dbt is available in the environment and DBT_PROFILES_DIR is correctly set
+downstream_models = []
+
+
+for model_name in changed_model_names:
+    try:
+        # Execute the dbt ls command
+        downstream_output = subprocess.check_output(['dbt', 'ls', '--select', model_name + '+', '--output', 'json'])
+        
+        # Split the output by lines and filter out non-JSON parts
+        for line in downstream_output.decode('utf-8').split('\n'):
+            try:
+                # Attempt to decode each line as JSON
+                json_line = json.loads(line)
+                downstream_models.append(json_line)
+            except json.JSONDecodeError:
+                # Skip lines that are not valid JSON
+                continue
+    except subprocess.CalledProcessError as e:
+        print(f"Error executing dbt ls for model {model_name}: {e.output.decode()}")
+        continue
+    
+# Remove duplicates
+unique_downstream_models = set([x.get("original_file_path") for x in downstream_models])
+
+# Print models for CI/CD usage
+print("DOWNSTREAM MODELS:")
+print(' '.join(unique_downstream_models))
+
+for model in unique_downstream_models:
+    subprocess.check_output(['dbt', 'run', '--select', 'model', '--vars', '{"schema_override": "dbt_test"}'])
--- models/intermediate/int_cashpayments.sql
+++ models/intermediate/int_cashpayments.sql
@@ -10,6 +10,7 @@ SELECT
     bcp.payment_method,
     bcp.doctor_id,
     bcp.patient_id,
+    
     bcp.parent_id
 
 FROM {{source( 'chronometer_scrubbed', 'billing_cashpayment' ) }} bcp
