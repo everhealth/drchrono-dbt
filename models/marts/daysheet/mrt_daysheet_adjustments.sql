0f51235f1b82a1675b924846e7f48233d4846282
Chris Johnson <cjohnson@updox.com>
2024-02-05 22:16:24 +0000

    Added staging table for practice_group_options, and added in logical check to filter eras based on verification setting

--- models/marts/daysheet/mrt_daysheet_adjustments.sql
+++ models/marts/daysheet/mrt_daysheet_adjustments.sql
@@ -1,13 +1,10 @@
 select
-    *,
-    case
-        when doc_verify_era_before_post is true and era_is_verified is false
-            then 'exclude_era_payment'
-        when doc_verify_era_before_post is true and era_is_verified is null
-            then 'exclude_era_payment'
-        else 'include_era_payment'
+    *
     end as include_era_payment_status
 from {{ ref("int_lineitems_transactions") }}
+inne join
+    {{ ref("stg_practice_group_options") }} as pgo
+    on lit.practice_group_id = pgo.practice_group_id
 where
     coalesce(appointment_status, '') not in (
         'No Show', 'Cancelled', 'Rescheduled'
@@ -21,5 +18,5 @@ where
     -- interest, 1 = deductible, 2 = coinsurance, 3 = copayment
     and lit_ins_paid = 0
     and lit_is_archived is false
-    and include_era_payment_status = 'include_era_payment'
     and datediff(day, lit_created_at, current_date) < 365
+    and (era_is_verified or not pgo.verify_era_before_post)
--- models/marts/daysheet/mrt_daysheet_credits.sql
+++ models/marts/daysheet/mrt_daysheet_credits.sql
@@ -1,13 +1,8 @@
-select
-    *,
-    case
-        when doc_verify_era_before_post is true and era_is_verified is false
-            then 'exclude_era_payment'
-        when doc_verify_era_before_post is true and era_is_verified is null
-            then 'exclude_era_payment'
-        else 'include_era_payment'
-    end as include_era_payment_status
-from {{ ref("int_lineitems_transactions") }}
+select *
+from {{ ref("int_lineitems_transactions") }} as lit
+inner join
+    {{ ref("stg_practice_group_options") }} as pgo
+    on lit.practice_group_id = pgo.practice_group_id
 where
     coalesce(appointment_status, '') not in (
         'No Show', 'Cancelled', 'Rescheduled'
@@ -16,5 +11,5 @@ where
     and lit_adjustment_reason in ('-3', '253', '225')
     -- adjustment_reasons: -3 = insurance payment, 225 = interest, 253 = sequestration
     and lit_is_archived is false
-    and include_era_payment_status = 'include_era_payment'
     and datediff(day, lit_created_at, current_date) < 365
+    and (era_is_verified or not pgo.verify_era_before_post)
