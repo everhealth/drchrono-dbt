54401b03986d059fb38e0902ba4bb1460424b17d
Alex Wickstrom <awickstrom@ucla.edu>
2024-02-06 14:01:59 -0800

    fix column references in debits

--- models/marts/daysheet/mrt_daysheet_debits.sql
+++ models/marts/daysheet/mrt_daysheet_debits.sql
@@ -3,9 +3,9 @@ from {{ ref("int_lineitems") }}
 where
     NOT (
         COALESCE(appointment_status, '') = 'No Show'
-        AND procedure_type IN ('C', 'H', 'R')
+        AND bli_procedure_type IN ('C', 'H', 'R')
     )
     AND COALESCE(appointment_status, '') NOT IN ('Cancelled', 'Rescheduled')
-    AND billed > 0
-    AND DATEDIFF(day, created_at, CURRENT_DATE) < 365
+    AND bli_billed > 0
+    AND DATEDIFF(day, bli_created_at, CURRENT_DATE) < 365
 {{ apply_limit_if_test() }}
