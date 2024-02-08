0cbfaae0f6ba1d2b131446ce561ae10731756748
Alex Wickstrom <awickstrom@ucla.edu>
2024-02-08 11:16:56 -0800

    pg_filter and days_ago

---
+++ macros/filter_pg.sql
@@ -0,0 +1,9 @@
+{%- macro filter_pg(tbl_alias) -%}
+
+    {% if env_var('DBT_ENVIRONMENT') == 'test' %}
+        {{ tbl_alias }}.practice_group_id = 1479
+    {% else %}
+        1=1
+    {% endif %}
+
+{%- endmacro -%}
