77a54bb8be476f24395aa1909cbb30fb29b14642
Alex Wickstrom <awickstrom@ucla.edu>
2024-02-08 12:25:19 -0800

    fix envvar reference

--- macros/test_runs/days_ago.sql
+++ macros/test_runs/days_ago.sql
@@ -1,6 +1,6 @@
 {% macro days_ago(col_name) %}
 
-    {% if env_var('DBT_ENVIRONMENT') == 'test' %}
+    {% if env_var('DBT_ENVIRONMENT', '') == 'test' %}
         AND {{ col_name }} > current_date - INTERVAL '90 day'
     {% endif %}
     
--- macros/test_runs/filter_pg.sql
+++ macros/test_runs/filter_pg.sql
@@ -1,6 +1,6 @@
 {%- macro filter_pg(tbl_alias) -%}
 
-    {% if env_var('DBT_ENVIRONMENT') == 'test' %}
+    {% if env_var('DBT_ENVIRONMENT', '') == 'test' %}
         {{ tbl_alias }}.practice_group_id = 1479
     {% else %}
         1=1
