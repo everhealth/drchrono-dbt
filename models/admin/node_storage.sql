12e6b300c7eaed478340a7ee9dcbd972980df9d5
Alex Wickstrom <awickstrom@ucla.edu>
2024-02-13 14:01:54 -0800

    enable 7-days history for node_storage.sql

--- models/admin/node_storage.sql
+++ models/admin/node_storage.sql
@@ -1,7 +1,33 @@
-SELECT
+{{
+  config(
+    materialized = "incremental",
+    post_hook="DELETE FROM {{ this }} WHERE snapshot_time < CURRENT_TIMESTAMP - INTERVAL '30 days'"
+  )
+}}
+WITH storage_data AS (
+  SELECT
     node,
     used,
     capacity,
-    round(used::decimal/capacity, 4) AS percent_used
-FROM stv_node_storage_capacity 
-ORDER BY node
+    ROUND(used::DECIMAL / capacity, 4) AS percent_used,
+    CURRENT_TIMESTAMP AS snapshot_time
+  FROM stv_node_storage_capacity
+)
+
+SELECT
+  node,
+  used,
+  capacity,
+  percent_used,
+  snapshot_time
+FROM storage_data
+-- WHERE  DATEDIFF(days, CURRENT_TIMESTAMP, snapshot_time) 
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  -- (uses > to include records whose timestamp occurred since the last run of this model)
+  WHERE snapshot_time > (select max(snapshot_time) from {{ this }})
+
+{% endif %}
+ORDER BY snapshot_time, node
+
