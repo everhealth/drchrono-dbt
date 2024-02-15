80dbac298a0df202d9d5fd53d49eefaac7fa24b4
Chris Johnson <cjohnson@updox.com>
2024-02-15 23:10:41 +0000

    incremental model changes

--- models/marts/daysheet/mrt_daysheet_cashpayments.sql
+++ models/marts/daysheet/mrt_daysheet_cashpayments.sql
@@ -1,22 +1,24 @@
 {{ config(
         SORT=["doctor_id", "posted_date", "appt_date_of_service"],
-        materialized='incremental'
+        materialized='incremental',
+        unique_key = 'cashpayment_id'
     ) 
 }}
 
+WITH fresh_data AS (
 
-select
+    SELECT
     -- CashPayment
-    bcp.cashpayment_id,
-    bcp.appointment_id,
-    bcp.line_item_id,
-    bcp.posted_date,
-    bcp.payment_date,
-    bcp.created_by_id,
-    bcp.amount,
-    bcp.trace_number,
-    bcp.payment_method,
-    bcp.parent_id,
+        bcp.cashpayment_id,
+        bcp.appointment_id,
+        bcp.line_item_id,
+        bcp.posted_date,
+        bcp.payment_date,
+        bcp.created_by_id,
+        bcp.amount,
+        bcp.trace_number,
+        bcp.payment_method,
+        bcp.parent_id,
     -- Patient
     {{ patient_fields("p") }},
     -- Doctor
@@ -35,27 +37,65 @@ select
     a.appt_institutional_claim_flag,
     bli.code as billing_code,
     appt_primary_insurer_company as ins_info_name,
-    appt_primary_insurer_payer_id as ins_info_payer_id
-from {{ ref("stg_cash_payments") }} as bcp
-left join
-    {{ ref("stg_appointments") }} as a
-    on bcp.appointment_id = a.appointment_id
-left join
-    {{ ref("stg_doctors") }} as d
-    on coalesce(a.doctor_id, bcp.doctor_id) = d.doctor_id
-left join
-    {{ ref("stg_patients") }} as p
-    on bcp.patient_id = p.patient_id
-left join {{ ref("stg_offices") }} as o on a.office_id = o.office_id
-left join
-    {{ ref("stg_line_items") }} as bli
-    on bcp.line_item_id = bli.line_item_id
+    appt_primary_insurer_payer_id as ins_info_payer_id,        
+    li_updated_at ,
+    appt_updated_at ,
+    doc_updated_at ,
+    office_updated_at,
+    patient_updated_at ,
+    cash_updated_at
+    FROM {{ ref("stg_cash_payments") }} AS bcp
+    LEFT JOIN
+        {{ ref("stg_appointments") }} AS a
+        ON bcp.appointment_id = a.appointment_id
+    LEFT JOIN
+        {{ ref("stg_doctors") }} AS d
+        ON coalesce(a.doctor_id, bcp.doctor_id) = d.doctor_id
+    LEFT JOIN
+        {{ ref("stg_patients") }} AS p
+        ON bcp.patient_id = p.patient_id
+    LEFT JOIN {{ ref("stg_offices") }} AS o ON a.office_id = o.office_id
+    LEFT JOIN
+        {{ ref("stg_line_items") }} AS bli
+        ON bcp.line_item_id = bli.line_item_id
 
-where
-    bcp.amount != 0
-    and coalesce(a.appointment_status, '') not in ('Cancelled', 'Rescheduled')
-    and datediff(day, GREATEST(bcp.posted_date, bcp.payment_date), current_date) < 365
-    {% if is_incremental() %}
-        and bcp.posted_date > (select max(bcp.posted_date) from {{ this }})
-    {% endif %}
+    WHERE
+        bcp.amount != 0
+        AND coalesce(a.appointment_status, '') NOT IN (
+            'Cancelled', 'Rescheduled'
+        )
+        AND datediff(
+            DAY, greatest(bcp.posted_date, bcp.payment_date), current_date
+        )
+        < 365
+)
+
+
+{% if is_incremental() %}
+,max_updated_at AS (
+    SELECT
+        max(li_updated_at) AS max_li_updated_at,
+        max(appt_updated_at) AS max_appt_updated_at,
+        max(doc_updated_at) AS max_doc_updated_at,
+        max(office_updated_at) AS max_office_updated_at,
+        max(patient_updated_at) AS max_patient_updated_at,
+        max(cash_updated_at) AS max_cash_updated_at
+    FROM {{ this }}
+)
+
+SELECT * FROM fresh_data
+    WHERE (
+        li_updated_at > max_li_updated_at
+        OR appt_updated_at > max_appt_updated_at
+        OR doc_updated_at > max_doc_updated_at
+        OR office_updated_at > max_office_updated_at
+        OR patient_updated_at > max_patient_updated_at
+        OR cash_updated_at > max_cash_updated_at
+    )
+{% else %}
+
+SELECT * FROM fresh_data
+
+{% endif %}
+
 {{ apply_limit_if_test() }}
--- models/marts/daysheet/mrt_daysheet_credits.sql
+++ models/marts/daysheet/mrt_daysheet_credits.sql
@@ -2,25 +2,56 @@
     config(
         SORT=["practice_group_id", "doctor_id"],
         materialized = "incremental",
+        unique_key = 'line_item_transaction_id'
     ) 
 }}
 
-select lit.*
-from {{ ref("int_lineitems_transactions") }} as lit
-inner join
-    {{ ref("stg_practice_group_options") }} as pgo
-    on lit.practice_group_id = pgo.practice_group_id
-where
-    coalesce(appointment_status, '') not in (
-        'No Show', 'Cancelled', 'Rescheduled'
+WITH fresh_data AS (
+    SELECT lit.*
+    FROM {{ ref("int_lineitems_transactions") }} AS lit
+    INNER JOIN
+        {{ ref("stg_practice_group_options") }} AS pgo
+        ON lit.practice_group_id = pgo.practice_group_id
+    WHERE
+        coalesce(appointment_status, '') NOT IN (
+            'No Show', 'Cancelled', 'Rescheduled'
+        )
+        AND lit_ins_paid != 0
+        AND lit_adjustment_reason IN ('-3', '253', '225')
+        -- adjustment_reasons: -3 = insurance payment, 225 = interest, 253 = sequestration
+        AND lit_is_archived IS false
+        AND greatest(lit_created_at, lit_posted_date, era_deposit_date)
+        > current_date - INTERVAL '365 days'
+        AND (era_is_verified OR NOT pgo.verify_era_before_post)
+)
+
+{% if is_incremental() %}
+,max_updated_at AS (
+    SELECT
+        max(li_updated_at) AS max_li_updated_at,
+        max(appt_updated_at) AS max_appt_updated_at,
+        max(doc_updated_at) AS max_doc_updated_at,
+        max(office_updated_at) AS max_office_updated_at,
+        max(patient_updated_at) AS max_patient_updated_at,
+        max(lit_updated_at) AS max_lit_updated_at,
+        max(era_updated_at) AS max_era_updated_at
+    FROM {{ this }}
+)
+
+SELECT * FROM fresh_data
+    WHERE (
+        li_updated_at > max_li_updated_at
+        OR appt_updated_at > max_appt_updated_at
+        OR doc_updated_at > max_doc_updated_at
+        OR office_updated_at > max_office_updated_at
+        OR patient_updated_at > max_patient_updated_at
+        OR lit_updated_at > max_lit_updated_at
+        OR era_updated_at > max_era_updated_at
     )
-    and lit_ins_paid != 0
-    and lit_adjustment_reason in ('-3', '253', '225')
-    -- adjustment_reasons: -3 = insurance payment, 225 = interest, 253 = sequestration
-    and lit_is_archived is false
-    and datediff(day, GREATEST(lit_created_at,lit_posted_date, era_deposit_date) , current_date) < 365
-    and (era_is_verified or not pgo.verify_era_before_post)
-    {% if is_incremental() %}
-        and lit_created_at > (select max(lit_created_at) from {{ this }})
-    {% endif %}
+{% else %}
+
+SELECT * FROM fresh_data
+
+{% endif %}
+
 {{ apply_limit_if_test() }}
