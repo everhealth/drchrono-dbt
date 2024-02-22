4b9c0e377183a85d8cf3b4c4a99be3af34c6495a
Chris Johnson <cjohnson@updox.com>
2024-02-22 19:48:47 +0000

    fix col references in aging ar details

--- models/marts/aging_ar/mrt_aging_ar_details.sql
+++ models/marts/aging_ar/mrt_aging_ar_details.sql
@@ -4,38 +4,41 @@
     ) 
 }}
 
-with lineitems as ( select * FROM  {{ ref("int_lineitems") }} ),
-cash_payments as ( select * FROM  {{ ref("stg_cash_payments") }} ),
+WITH lineitems AS (SELECT * FROM {{ ref("int_lineitems") }})
 
-final as (SELECT li.line_item_id
-    --patient
-    ,{{ patient_fields("li") }}
-    ,li.primary_insurance_company
-    -- doctor
-    ,{{ doctor_fields("li") }}
-    ,{{ office_fields("li") }}
-   ,li.practice_group_id
-  , li.balance_ins
-  , li.balance_pt
-  , li.billed
-  , li.allowed 
-  , li.price
-  , li.quantity
-  , li.adjustment
-  , li.expected_reimbursement 
-  , li.procedure_type
-  , li.billing_code
-  , li.li_created_at
-  , li.first_billed_date 
-  , li.last_billed_date
-  , li.date_of_service
-  , li.appointment_id
-  , li.appt_billing_status
+, cash_payments AS (SELECT * FROM {{ ref("stg_cash_payments") }})
 
-FROM lineitems AS li
-LEFT JOIN cash_payments AS bcp
-    ON bcp.line_item_id = li.line_item_id
-WHERE (li.balance_ins != 0 OR li.balance_pt != 0)
+, final AS (
+    SELECT
+        li.line_item_id
+        --patient
+        , {{ patient_fields("li") }}
+        , li.primary_insurance_company
+        -- doctor
+        , {{ doctor_fields("li") }}
+        , {{ office_fields("li") }}
+        , li.practice_group_id
+        , li.balance_ins
+        , li.balance_pt
+        , li.billed
+        , li.li_allowed
+        , li.price
+        , li.quantity
+        , li.li_adjustment
+        , li.expected_reimbursement
+        , li.procedure_type
+        , li.billing_code
+        , li.li_created_at
+        , li.first_billed_date
+        , li.last_billed_date
+        , li.date_of_service
+        , li.appointment_id
+        , li.appt_billing_status
+
+    FROM lineitems AS li
+        LEFT JOIN cash_payments AS bcp
+            ON li.line_item_id = bcp.cash_line_item_id
+    WHERE (li.balance_ins != 0 OR li.balance_pt != 0)
 )
 
 SELECT * FROM final
