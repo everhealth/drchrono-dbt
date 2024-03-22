95cdb1c9e00eeb6aca70d1b4f37d939664a4d274
Alex Wickstrom <awickstrom@ucla.edu>
2024-03-22 12:29:12 -0700

    Undo timezone conversion for ERAs

--- models/staging/stg_era_objects.sql
+++ models/staging/stg_era_objects.sql
@@ -6,9 +6,9 @@ SELECT
     , is_verified                                  AS era_is_verified
     , is_eob
     , is_archived                                  AS era_is_archived
-    , CONVERT_TIMEZONE('EST', 'UTC', posted_date)  AS era_posted_date
-    , CONVERT_TIMEZONE('EST', 'UTC', created_at)   AS era_created_at
-    , CONVERT_TIMEZONE('EST', 'UTC', updated_at)   AS era_updated_at
-    , CONVERT_TIMEZONE('EST', 'UTC', deposit_date) AS era_deposit_date
+    , posted_date                                  AS era_posted_date   -- stored in EST TZ
+    , created_at                                   AS era_created_at    -- stored in EST TZ
+    , updated_at                                   AS era_updated_at    -- stored in EST TZ
+    , deposit_date                                 AS era_deposit_date  -- stored in EST TZ
 FROM {{ source("chronometer_production", "billing_eraobject") }}
 WHERE _fivetran_deleted IS FALSE
