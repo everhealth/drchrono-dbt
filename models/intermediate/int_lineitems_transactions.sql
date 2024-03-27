4d07fa7a127a77cf10786682e2be5d2e03ba96c7
Chris Johnson <cjohnson@updox.com>
2024-03-27 13:57:06 +0000

    Updated join on int_lineitems_transactions for when a line_item has been deleted, but we need to tie transaction to appt.

--- models/intermediate/int_lineitems_transactions.sql
+++ models/intermediate/int_lineitems_transactions.sql
@@ -24,7 +24,32 @@ WITH
                     THEN li.secondary_insurer_payer_id
             END AS ins_info_payer_id
         FROM line_item_transactions AS lit
-            LEFT JOIN line_items AS li ON lit.lit_line_item_id = li.line_item_id
+            LEFT JOIN line_items AS li
+                ON lit.lit_line_item_id IS NOT NULL AND lit.lit_line_item_id = li.line_item_id
+            LEFT JOIN era_objects AS era ON lit.lit_era_id = era.era_id
+        WHERE lit.lit_line_item_id IS NULL OR li.line_item_id IS NOT NULL
+
+        UNION ALL
+
+        SELECT
+            li.*
+            , era.*
+            , lit.*
+            , CASE
+                WHEN ins_idx = 1
+                    THEN li.primary_insurer_company
+                WHEN ins_idx = 2
+                    THEN li.secondary_insurer_company
+            END AS ins_info_name
+            , CASE
+                WHEN ins_idx = 1
+                    THEN li.primary_insurer_payer_id
+                WHEN ins_idx = 2
+                    THEN li.secondary_insurer_payer_id
+            END AS ins_info_payer_id
+        FROM line_item_transactions AS lit
+            LEFT JOIN line_items AS li
+                ON lit.lit_line_item_id IS NULL AND lit.lit_appointment_id = li.appointment_id
             LEFT JOIN era_objects AS era ON lit.lit_era_id = era.era_id
         WHERE lit.lit_line_item_id IS NULL OR li.line_item_id IS NOT NULL
     )
