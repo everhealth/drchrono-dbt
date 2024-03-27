3cffd950c6db3ab08b3b1fc1cad5a86002570f41
Chris Johnson <74216727+ChrisAlertMD@users.noreply.github.com>
2024-03-27 10:29:30 -0500

    Daysheet credits join (#68)
    * Merge staging to main branch (#65)
    
    * update staging Redshift user
    
    * edits to profiles and deployments (#62)
    
    * edits to profiles and deployments
    
    * updated user
    
    * make target a variable
    
    * remove quotes
    
    * Revert "remove quotes"
    
    This reverts commit 5a9ca97983665998f94d117b9f3161f2e78ee6c9.
    
    * remove target, should be overridden in fivetran
    
    * revert threads
    
    * testing old dev creds and envars
    
    * update dev envvars
    
    * Update profiles.yml
    
    * Update profiles.yml
    
    * Update deployment.yml
    
    * materialize upstream models for daysheet daily_refresh
    
    * cleaning up daily refresh, scheduling aging ar
    
    * trying posthook to grant access
    
    * fix posthook ref
    
    * Daysheet fixes (#64)
    
    * Staging (#63)
    
    * update staging Redshift user
    
    * edits to profiles and deployments (#62)
    
    * edits to profiles and deployments
    
    * updated user
    
    * make target a variable
    
    * remove quotes
    
    * Revert "remove quotes"
    
    This reverts commit 5a9ca97983665998f94d117b9f3161f2e78ee6c9.
    
    * remove target, should be overridden in fivetran
    
    * revert threads
    
    * testing old dev creds and envars
    
    * update dev envvars
    
    ---------
    
    Co-authored-by: Alex Wickstrom <alex.wickstrom@drchrono.com>
    
    * add union all and grain ids for daysheet summary
    
    ---------
    
    Co-authored-by: Alex Wickstrom <alex.wickstrom@drchrono.com>
    
    * adding posthook for perms on all tables
    
    * fix column order of summary
    
    * removing days_ago macro from int_lineitems
    
    ---------
    
    Co-authored-by: Alex Wickstrom <alex.wickstrom@drchrono.com>
    Co-authored-by: Chris Johnson <74216727+ChrisAlertMD@users.noreply.github.com>
    Co-authored-by: Chris Johnson <cjohnson@updox.com>
    
    * Updated join on int_lineitems_transactions for when a line_item has been deleted, but we need to tie transaction to appt.
    
    ---------
    
    Co-authored-by: Alex Wickstrom <awickstrom@ucla.edu>
    Co-authored-by: Alex Wickstrom <alex.wickstrom@drchrono.com>

--- models/staging/stg_line_item_transactions.sql
+++ models/staging/stg_line_item_transactions.sql
@@ -1,21 +1,22 @@
 SELECT
-    id                                            AS line_item_transaction_id
-    , line_item_id                                AS lit_line_item_id
-    , era_id                                      AS lit_era_id
+    id               AS line_item_transaction_id
+    , line_item_id   AS lit_line_item_id
+    , era_id         AS lit_era_id
+    , appointment_id AS lit_appointment_id
     , trace_number
-    , claim_status                                AS lit_claim_status
+    , claim_status   AS lit_claim_status
     , adjustment_group_code
-    , allowed                                     AS lit_allowed
-    , adjustment                                  AS lit_adjustment
+    , allowed        AS lit_allowed
+    , adjustment     AS lit_adjustment
     , ins_paid
     , adjustment_reason
     , adjusted_adjustment_reason
     , code
-    , status                                      AS lit_status
-    , is_archived                                 AS lit_is_archived
+    , status         AS lit_status
+    , is_archived    AS lit_is_archived
     , ins_idx
-    , updated_at                                  AS lit_updated_at
-    , posted_date                                 AS lit_posted_date  -- stored in EST TZ
-    , created_at                                  AS lit_created_at   -- stored in EST TZ
+    , updated_at     AS lit_updated_at
+    , posted_date    AS lit_posted_date  -- stored in EST TZ
+    , created_at     AS lit_created_at   -- stored in EST TZ
 FROM {{ source("chronometer_production", "billing_lineitemtransaction") }}
 WHERE _fivetran_deleted IS FALSE
