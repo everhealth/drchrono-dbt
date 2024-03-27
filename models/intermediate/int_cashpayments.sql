ce8e03553f139f1c62a07f743720895c3658d3db
Chris Johnson <cjohnson@updox.com>
2024-03-27 15:12:40 -0500

    revert changes made to models/intermediate/int_cashpayments.sql
    reverting changes, as cash payments are often not tied to appts

--- models/intermediate/int_cashpayments.sql
+++ models/intermediate/int_cashpayments.sql
@@ -1,7 +1,7 @@
 {{
     config(
         SORT=[
-            "bli_created_at",
+            "cashpayment_id",
             "date_of_service",
             "practice_group_id",
             "doctor_id",
@@ -11,22 +11,33 @@
 
 
 WITH
+    cash_payments AS (SELECT * FROM {{ ref("stg_cash_payments") }})
 
-    appointment_info AS (SELECT * FROM {{ ref("int_appointments") }})
+    , appointments AS (SELECT * FROM {{ ref("stg_appointments") }})
 
-    , cash_payments AS (SELECT * FROM {{ ref("stg_cash_payments") }})
+    , doctors AS (SELECT * FROM {{ ref("stg_doctors") }})
+
+    , patients AS (SELECT * FROM {{ ref("stg_patients") }})
+
+    , offices AS (SELECT * FROM {{ ref("stg_offices") }})
 
     , line_items AS (SELECT * FROM {{ ref("stg_line_items") }})
 
     , final AS (
-
         SELECT
 
             cp.*
-            , ai.*
+            , a.*
+            , d.*
+            , p.*
+            , o.*
             , li.*
+            , {{ exam_room_name("a","o") }}
         FROM cash_payments AS cp
-            LEFT JOIN appointment_info AS ai ON cp.cash_appointment_id = ai.appointment_id
+            LEFT JOIN appointments AS a ON cp.cash_appointment_id = a.appointment_id
+            LEFT JOIN doctors AS d ON COALESCE(a.appt_doctor_id, cp.cash_doctor_id) = d.doctor_id
+            LEFT JOIN patients AS p ON cp.cash_patient_id = p.patient_id
+            LEFT JOIN offices AS o ON a.appt_office_id = o.office_id
             LEFT JOIN line_items AS li ON cp.cash_line_item_id = li.line_item_id
     )
 
